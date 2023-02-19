01263ba540b5a3e10eb9c8c0a99011782dc32bdc
Bart Schelpe <bart.schelpe@gmail.com>
2023-02-19 13:57:46 +0100

    ➕ Added bot login event

--- bot.py
+++ bot.py
@@ -17,5 +17,15 @@ DEBUG_GUILD = os.getenv('DEBUG_GUILD')
 bot = discord.Bot(debug_guilds=[DEBUG_GUILD])
 bot_version = "1.0"
 
+# When the bot is ready, print a message to the console, count the guilds and print the number of guilds
+@bot.event
+async def on_ready():
+	print(f"We have logged in as {bot.user} v{bot_version}")
+	guild_count = 0
+	for guild in bot.guilds:
+		print(f"- {guild.id} (name: {guild.name})")
+		guild_count = guild_count + 1
+	print("Trackbot is in " + str(guild_count) + " guilds.")
+
 # Run the bot with the token
 bot.run(TOKEN)
